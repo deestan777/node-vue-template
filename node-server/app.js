aaed86c563d8294e74a1551545706471378af7fa
deestan777 <deestan@ymail.com>
2022-12-27 07:48:31 +0200

    first commit

---
+++ node-server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const serverLog = require('easy-log')('app:server');
+const cors = require('cors');
+
+const app = express();
+
+app.use((req,res,next)=>{
+    serverLog(`Request: ${req.method} - ${req.originalUrl}`);
+    next();
+})
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors());
+
+app.get('/', (req,res,next) => {
+    res.send('Hello World');
+})
+module.exports = app;
---
+++ vue-client/src/components/Blog.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+  <h1>Blog</h1>
+  <p class="lead">{{fetch_status}}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import response from "core-js/internals/is-forced";
+export default {
+  name: 'Blog',
+  data() {
+    return {
+      fetch_status: 'Fetching data from server...',
+    }
+  },
+  created () {
+  axios({
+    method: 'get',
+    url: 'http://localhost:9000/',
+  })
+      .then((response) => {
+        this.fetch_status = 'Data fetched from server: ' + response.data
+      })
+      .catch((error) => {
+        this.fetch_status = 'Error fetching data from server: ' + error
+      })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
